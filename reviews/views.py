f86e40857c19180fb65ebb653641ae4350168084
Vaibhav Reddy <vaibhavreddy0852@gmail.com>
2024-12-27 08:04:21 +0530

    added updating existing entry

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
 from django.views import View
+from .models import Review
 from . import forms
 
 # Create your views here.
@@ -12,13 +13,21 @@ class ReviewView(View):
     
 
     def post(self, request):
-        form = forms.ReviewForm(request.POST)
+        user_name = request.POST.get('user_name')
+        review = Review.objects.filter(user_name=user_name).first()
+        
+        if review:
+            form = forms.ReviewForm(request.POST, instance=review)
+        else:
+            form = forms.ReviewForm(request.POST)
+        
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/thank-you')
-        return render(request, 'reviews/review.html',{
-                'form': form
-            })
+        
+        return render(request, 'reviews/review.html', {
+            'form': form
+        })
 
 def thank_you(request):
     return render(request, 'reviews/thank_you.html')
